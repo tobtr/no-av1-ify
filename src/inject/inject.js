4d1f3467cc841a92536e2081dc47fbb508626453
TobTr <93725029+tobtr@users.noreply.github.com>
2026-09-09 10:48:21 +0700

    Restrict disallowed video types to 'av01' only

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -59,7 +59,7 @@
     // Check if a video type is allowed
     return function (type) {
       if (type === undefined) return '';
-      var disallowed_types = ['webm', 'vp8', 'vp9', 'av01'];
+      var disallowed_types = ['av01'];
       // If video type is in disallowed_types, say we don't support them
       for (var i = 0; i < disallowed_types.length; i++) {
         if (type.indexOf(disallowed_types[i]) !== -1) return '';
